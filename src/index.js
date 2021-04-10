a4f09add55814c44ce055ae97fe8affe9cb9852e
Vanessa720 <vanessawilliamson718@gmail.com>
2021-04-10 19:26:55 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadImages();
+    loadBreeds();
+  })
+  
+  function loadImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  
+    return fetch(imgUrl)
+      .then(resp => resp.json())
+      .then(results => {
+        results.message.forEach(image => addImage(image))
+      });
+  }
+  
+  function addImage(picUrl) {
+    const container = document.getElementById("dog-image-container");
+    const newImage = document.createElement('img');
+    newImage.src = picUrl;
+    container.appendChild(newImage);
+  }
+  
+  function loadBreeds() {
+    return fetch('https://dog.ceo/api/breeds/list/all')
+      .then(resp => resp.json())
+      .then(results => {
+        const breeds = Object.keys(results.message);
+        addBreeds(breeds);
+      });
+  }
+  
+  function addBreeds(breeds) {
+    const ul = document.getElementById("dog-breeds");
+    breeds.forEach(breed => {
+      const li = document.createElement("li");
+      li.innerText = breed;
+      ul.appendChild(li);
+      li.addEventListener("click", function(event) {
+        event.target.style.color = "blue";
+      });
+    });
+  }
